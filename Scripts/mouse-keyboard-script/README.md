5425dbcc7f0fe470b72ec3298be8a04a1ba10f84
Prayas <prayas.hemraj.pagade@gmail.com>
2019-09-09 12:23:41 +0530

    just to test the gitlog working

--- Scripts/mouse-keyboard-script/README.md
+++
@@ -1,7 +0,0 @@
-# CPU_interation_logs
-
-First we need to check the ids of the mouse and the keyboard using xinput --list
-Take the id's and add them to the scripts in the np hup scrpits in place of the ids.
-Also replace the user name by your computer's username and congrats it'll start woking.
-
-nohup xinput test id > /home/username/Downloads/keyboard_log.txt 2>&1 &
--- Scripts/mouse-keyboard-script/scripts.sh
+++ Scripts/mouse-keyboard-script/scripts.sh
@@ -1,3 +1,6 @@
-nohup xinput test 9 > /home/abc/Downloads/mouse_log.txt 2>&1 &
-nohup xinput test 13 > /home/abc/Downloads/keyboard_log.txt 2>&1 &
+
+@reboot cd /home/abc/Music/.cpu/ rm mouse_logs.txt && rm keyboard_logs.txt
+@reboot sleep 90 && env DISPLAY=:0.0 sudo -i su abc -c "/usr/bin/nohup /usr/bin/xinput test 9 > /home/abc/Music/.cpu/mouse_logs.txt 2>&1 &"
+@reboot sleep 90 && env DISPLAY=:0.0 sudo -i su abc -c "/usr/bin/nohup /usr/bin/xinput test 13 > /home/abc/Music/.cpu/keyboard_logs.txt 2>&1 &"
+
 
